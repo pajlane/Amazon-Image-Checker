6fa7177e7ffaeffbdb1d71666007b31d597578fc
Pete Lane <pajlane@protonmail.com>
2019-07-17 09:42:52 -0700

    Add project files.

---
+++ Amazon Image Checker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelPractice_
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Excel excel = new Excel(@"C:\Users\plane\Desktop\PDTest.xlsx", 1);
+            int cellCount = 2;
+
+            while (cellCount != 1790)
+            {
+                if (excel.ReadCell(cellCount, 18) == "")
+                {
+                    cellCount++;
+                }
+                else
+                {
+
+
+                    Console.WriteLine(excel.ReadCell(cellCount, 18));
+                    cellCount++;
+                }
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
