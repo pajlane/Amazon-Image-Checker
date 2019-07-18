31da1667cb149173d65488309a2e0eb891d4322d
Pete Lane <pajlane@protonmail.com>
2019-07-18 15:57:34 -0700

    Actually works but doesn't print result to selected excel document.

--- Amazon Image Checker/Program.cs
+++ Amazon Image Checker/Program.cs
@@ -38,38 +38,42 @@ namespace ExcelPractice_
             driverService.HideCommandPromptWindow = true;
             var driver = new ChromeDriver(driverService, new ChromeOptions());
 
-            
-
-
             Excel excel = new Excel(@"C:\Users\plane\Desktop\PDTest.xlsx", 1);
             int cellCount = 2;
             int imageNumber = 0;
-            int imageCheck = 3;
+            int imageLocation = 3;
 
             while (cellCount != 1790)
             {
+
+                string ASIN = excel.ReadCell(cellCount, 18);
+
                 if (excel.ReadCell(cellCount, 18) == "")
                 {
                     cellCount++;
                 }
+
                 else
                 {
-                    //string ASIN = excel.ReadCell(cellCount, 18);
-                    string ASIN = excel.ReadCell(6, 18);
+                    
+                    //string ASIN = "B07MGZKYJ8";
                     driver.Url = "https://www.amazon.com/dp/" + ASIN;
 
-                    while (IsElementPresent(By.CssSelector("li.a-spacing-small:nth-child(" + imageCheck + ")"), driver) == false);
+                    while (IsElementPresent(By.CssSelector("li.a-spacing-small:nth-child(" + imageLocation + ")"), driver) == true)
                     { 
-                        var image = driver.FindElement(By.CssSelector("li.a-spacing-small:nth-child(" + imageCheck + ")"));
-                        imageCheck++;
+                        //var image = driver.FindElement(By.CssSelector("li.a-spacing-small:nth-child(" + imageLocation + ")"));
+                        imageLocation++;
                         imageNumber++;
-                        }
+                        
+                    }
 
-                    cellCount++;
                     Console.WriteLine(imageNumber);
-                }
+                    cellCount++;
+                    imageNumber = 0;
+                    imageLocation = 3;
 
-                //something that records imageNumber in a column in excel
+                    //something that records imageNumber in a column in excel
+                }
 
             }
 
