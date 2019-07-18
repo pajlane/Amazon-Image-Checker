e261e6ebf758206b7dfa9233b794586edf542a44
Pete Lane <pajlane@protonmail.com>
2019-07-18 13:40:37 -0700

    Basic functionality

--- Amazon Image Checker/Program.cs
+++ Amazon Image Checker/Program.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace ExcelPractice_
 {
     class Program
     {
-        public static void Main(string[] args)
+
+        private static bool IsElementPresent(By by, IWebDriver driver)
+        {                                                       
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            catch (ElementNotVisibleException)
+            {
+                return false;
+            }
+        }
+
+
+            public static void Main(string[] args)
         {
+
+            var driverService = ChromeDriverService.CreateDefaultService(@"\\brmpro\MACAPPS\ClickOnce\CustomerServiceAutomationTool");
+            driverService.HideCommandPromptWindow = true;
+            var driver = new ChromeDriver(driverService, new ChromeOptions());
+
+            
+
 
             Excel excel = new Excel(@"C:\Users\plane\Desktop\PDTest.xlsx", 1);
             int cellCount = 2;
+            int imageNumber = 0;
+            int imageCheck = 3;
 
             while (cellCount != 1790)
             {
@@ -22,11 +54,22 @@ namespace ExcelPractice_
                 }
                 else
                 {
+                    //string ASIN = excel.ReadCell(cellCount, 18);
+                    string ASIN = excel.ReadCell(6, 18);
+                    driver.Url = "https://www.amazon.com/dp/" + ASIN;
 
+                    while (IsElementPresent(By.CssSelector("li.a-spacing-small:nth-child(" + imageCheck + ")"), driver) == false);
+                    { 
+                        var image = driver.FindElement(By.CssSelector("li.a-spacing-small:nth-child(" + imageCheck + ")"));
+                        imageCheck++;
+                        imageNumber++;
+                        }
 
-                    Console.WriteLine(excel.ReadCell(cellCount, 18));
                     cellCount++;
+                    Console.WriteLine(imageNumber);
                 }
+
+                //something that records imageNumber in a column in excel
 
             }
 
